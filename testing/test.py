da4804d1f09b6883bc44b5eebeb514a71980a023
M-Usamah <mohammad.usammah127@hotmail.com>
2022-07-19 22:40:25 +0500

    working

--- testing/test.py
+++ testing/test.py
@@ -1,18 +1,18 @@
-from distutils.errors import PreprocessError
 import torch
 import numpy as np
 import cv2
-
+from PIL import image
+from torchvision import  transforms
 
 model = torch.jit.load('scriptmodule.pt')
 model.eval()
 
-g = open("ls.txt", "r")
-b = g.read()
-data_into_list = b.split("\n")
-g.close()
+# g = open("ls.txt", "r")
+# b = g.read()
+# data_into_list = b.split("\n")
+# g.close()
 
-print(data_into_list[1])
+# print(data_into_list[1])
 
 
 width = 800
@@ -21,6 +21,18 @@ height = 800
 cam = cv2.VideoCapture(0)
 cam.set(4,width)
 cam.set(3,height)
+
+size = (64,64)
+trans = transforms.Compose(
+    [
+        transforms.Resize(size=size),
+        # transforms.CenterCrop(50),
+        # transforms.ColorJitter(brightness=.5,contrast=.4,hue=.6),
+        transforms.RandomEqualize(),
+        transforms.ToTensor(),
+        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ]
+)
 
 def perProcessing(img):
     img = cv2.equalizeHist(img)
